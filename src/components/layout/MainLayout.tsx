15597d82219364f0a1d3579bd65764853df58f45
Eduardo Gonçalves <eduardo.ga@estudante.iftm.edu.br>
2025-11-09 18:15:51 -0300

    feat: Sidebar finalizado com função de Logout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import { Sidebar } from "./Sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { ConfirmModal } from "../ConfirmModal";
 
 export function MainLayout(){
+    const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false)
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('authToken');
+
+        setIsLogoutModalOpen(false);
+
+        navigate('/login');
+    }
     return(
-        <div className="flex h-screen w-screen bg-slate-100">
-            <Sidebar />
-      
-        <main className="flex-1 flex flex-col overflow-auto">
-            
-            {/* (Aqui será adicionado o Header.tsx depois) */}
-            
-            <div className="p-8 ">
-            <Outlet />
-            </div>
-        </main>
-    </div>
-    )
+            <>
+            <div className="flex h-screen w-screen bg-slate-100">
+                <Sidebar onLogoutClick={() => setIsLogoutModalOpen(true)} />
+        
+            <main className="flex-1 flex flex-col overflow-auto">
+                <div className="p-8 ">
+                <Outlet />
+                </div>
+            </main>
+        </div>
+        <ConfirmModal
+            isOpen={isLogoutModalOpen}
+            onClose={() => setIsLogoutModalOpen(false)} 
+            onConfirm={handleLogout} 
+        >
+            {/* A Mensagem que você quer mostrar */}
+            <h2 className="text-xl font-bold text-gray-800">Confirmar Saída</h2>
+            <p className="mt-2 text-gray-600">
+            Você tem certeza que deseja sair do sistema?
+            </p>
+        </ConfirmModal>
+        </>
+    );
 }
